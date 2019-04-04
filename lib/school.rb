3e9bb182ba5a831020c59727e0dcaf9984a1b105
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-04 13:54:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+
+class School
+  attr_reader :name, :roster
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+end
